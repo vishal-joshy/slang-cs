dadc4b67edb3536d817315b8dee57e30886b532b
vishaljoshy18 <1812-vishal@protonmail.com>
2022-06-26 23:09:41 +0530

    Refactor binary expression.

--- Interpreter.cs
+++ Interpreter.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 
-namespace SLANG
-{
-  public interface Visitor
-  {
+namespace SLANG {
+  public interface Visitor {
     Symbol visit(RuntimeContext rtx, PrintStatement ps);
     Symbol visit(RuntimeContext rtx, PrintLineStatement pl);
     Symbol visit(RuntimeContext rtx, AssignmentStatement astmt);
@@ -11,10 +9,7 @@ namespace SLANG
     Symbol visit(RuntimeContext rtx, NumericConstant n);
     Symbol visit(RuntimeContext rtx, BooleanConstant b);
     Symbol visit(RuntimeContext rtx, StringLiteral s);
-    Symbol visit(RuntimeContext rtx, BinaryPlus bp);
-    Symbol visit(RuntimeContext rtx, BinaryMinus bm);
-    Symbol visit(RuntimeContext rtx, BinaryMultiplication bm);
-    Symbol visit(RuntimeContext rtx, BinaryDivision bd);
+    Symbol visit(RuntimeContext rtx, BinaryExpression bp);
     Symbol visit(RuntimeContext rtx, UnaryPlus up);
     Symbol visit(RuntimeContext rtx, UnaryMinus um);
     Symbol visit(RuntimeContext rtx, Variable v);
@@ -24,28 +19,25 @@ namespace SLANG
     Symbol visit(RuntimeContext rtx, IfStatement ifs);
     Symbol visit(RuntimeContext rtx, WhileStatment ws);
     Symbol visit(RuntimeContext rtx, ReturnStatement rs);
+    Symbol visit(RuntimeContext rtx, CallProcedureExpression cpe);
   }
 
-  public class Interpreter : Visitor
-  {
-    public Symbol visit(RuntimeContext rtx, PrintStatement ps)
-    {
+  public class Interpreter : Visitor {
+    public Symbol visit(RuntimeContext rtx, PrintStatement ps) {
       Expression exp = ps.GetExpression();
       Symbol result = exp.accept(rtx, this);
       Console.Write(result.GetValueAsString());
       return null;
     }
 
-    public Symbol visit(RuntimeContext rtx, PrintLineStatement pls)
-    {
+    public Symbol visit(RuntimeContext rtx, PrintLineStatement pls) {
       Expression exp = pls.GetExpression();
       Symbol result = exp.accept(rtx, this);
       Console.WriteLine(result.GetValueAsString());
       return null;
     }
 
-    public Symbol visit(RuntimeContext rtx, AssignmentStatement astmt)
-    {
+    public Symbol visit(RuntimeContext rtx, AssignmentStatement astmt) {
       Variable var = astmt.GetVariable();
       Expression exp = astmt.GetExpression();
       Symbol result = exp.accept(rtx, this);
@@ -53,199 +45,130 @@ namespace SLANG
       return null;
     }
 
-    public Symbol visit(RuntimeContext rtx, VariableDeclarationStatement vds)
-    {
+    public Symbol visit(RuntimeContext rtx, VariableDeclarationStatement vds) {
       Symbol s = vds.GetInfo();
       rtx.TABLE.Add(s);
       vds.Var = new Variable(s);
       return null;
     }
-    public Symbol visit(RuntimeContext rtx, NumericConstant n)
-    {
+    public Symbol visit(RuntimeContext rtx, NumericConstant n) {
       return n.GetSymbol();
     }
-    public Symbol visit(RuntimeContext rtx, BooleanConstant b)
-    {
+    public Symbol visit(RuntimeContext rtx, BooleanConstant b) {
       return b.GetSymbol();
     }
-    public Symbol visit(RuntimeContext rtx, StringLiteral s)
-    {
+    public Symbol visit(RuntimeContext rtx, StringLiteral s) {
       return s.GetSymbol();
     }
 
-    public Symbol visit(RuntimeContext rtx, BinaryPlus bp)
-    {
+    public Symbol visit(RuntimeContext rtx, BinaryExpression bp) {
       Symbol lEval = bp.GetLExpression().accept(rtx, this);
       Symbol rEval = bp.GetRExpression().accept(rtx, this);
+      OPERATOR op = bp.GetOperator();
 
-      if (lEval.Type == TYPE.NUMERIC && rEval.Type == TYPE.NUMERIC)
-      {
-        Symbol result = new Symbol();
-        result.Type = TYPE.NUMERIC;
-        result.DoubleValue = lEval.DoubleValue + rEval.DoubleValue;
-        result.Name = "";
-        return result;
+      if (lEval.Type != rEval.Type) {
+        throw new Exception("Type mismatch for binary operation");
       }
-      else if (lEval.Type == TYPE.STRING && rEval.Type == TYPE.STRING)
-      {
+
+      if (lEval.Type == TYPE.NUMERIC) {
+        Symbol result = new Symbol();
+        result.Type= TYPE.NUMERIC;
+        result.Name = "";
+        switch (op) {
+          case OPERATOR.PLUS: result.DoubleValue = lEval.DoubleValue + rEval.DoubleValue; return result;
+          case OPERATOR.MINUS: result.DoubleValue = lEval.DoubleValue - rEval.DoubleValue; return result;
+          case OPERATOR.MULT: result.DoubleValue = lEval.DoubleValue * rEval.DoubleValue; return result;
+          case OPERATOR.DIV: result.DoubleValue = lEval.DoubleValue / rEval.DoubleValue; return result;
+          default: throw new Exception("Invalid operator");
+        }
+      } else if (lEval.Type == TYPE.STRING) {
         Symbol result = new Symbol();
         result.Type = TYPE.STRING;
         result.StringValue = lEval.StringValue + rEval.StringValue;
         result.Name = "";
         return result;
-      }
-      else
-      {
+      } else {
         throw new Exception("Invalid types for binary plus");
       }
     }
 
-    public Symbol visit(RuntimeContext rtx, BinaryMinus bm)
-    {
-      Symbol lEval = bm.GetLExpression().accept(rtx, this);
-      Symbol rEval = bm.GetRExpression().accept(rtx, this);
-
-      if (lEval.Type == TYPE.NUMERIC && rEval.Type == TYPE.NUMERIC)
-      {
-        Symbol result = new Symbol();
-        result.Type = TYPE.NUMERIC;
-        result.DoubleValue = lEval.DoubleValue - rEval.DoubleValue;
-        result.Name = "";
-        return result;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary minus");
-      }
-    }
-
-    public Symbol visit(RuntimeContext rtx, BinaryMultiplication bm)
-    {
-      Symbol lEval = bm.GetLExpression().accept(rtx, this);
-      Symbol rEval = bm.GetRExpression().accept(rtx, this);
-
-      if (lEval.Type == TYPE.NUMERIC && rEval.Type == TYPE.NUMERIC)
-      {
-        Symbol result = new Symbol();
-        result.Type = TYPE.NUMERIC;
-        result.DoubleValue = lEval.DoubleValue * rEval.DoubleValue;
-        result.Name = "";
-        return result;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary multiplication");
-      }
-    }
-
-    public Symbol visit(RuntimeContext rtx, BinaryDivision bd)
-    {
-      Symbol lEval = bd.GetLExpression().accept(rtx, this);
-      Symbol rEval = bd.GetRExpression().accept(rtx, this);
-
-      if (lEval.Type == TYPE.NUMERIC && rEval.Type == TYPE.NUMERIC)
-      {
-        Symbol result = new Symbol();
-        result.Type = TYPE.NUMERIC;
-        result.DoubleValue = lEval.DoubleValue / rEval.DoubleValue;
-        result.Name = "";
-        return result;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary division");
-      }
-    }
-
-    public Symbol visit(RuntimeContext rtx, Variable v)
-    {
-      if (rtx.TABLE == null)
-      {
+    public Symbol visit(RuntimeContext rtx, Variable v) {
+      if (rtx.TABLE == null) {
         return null;
-      }
-      else
-      {
+      } else {
         Symbol result = rtx.TABLE.Get(v.GetName());
         return result;
       }
     }
 
-    public Symbol visit(RuntimeContext rtx, UnaryPlus up)
-    {
+    public Symbol visit(RuntimeContext rtx, UnaryPlus up) {
       Symbol eval = up.GetExpression().accept(rtx, this);
-      if (eval.Type == TYPE.NUMERIC)
-      {
+      if (eval.Type == TYPE.NUMERIC) {
         Symbol result = new Symbol();
         result.Type = TYPE.NUMERIC;
         result.DoubleValue = eval.DoubleValue;
         result.Name = "";
         return result;
-      }
-      else
-      {
+      } else {
         throw new Exception("Invalid type for unary plus");
       }
     }
-    public Symbol visit(RuntimeContext rtx, UnaryMinus um)
-    {
+    public Symbol visit(RuntimeContext rtx, UnaryMinus um) {
       Symbol eval = um.GetExpression().accept(rtx, this);
-      if (eval.Type == TYPE.NUMERIC)
-      {
+      if (eval.Type == TYPE.NUMERIC) {
         Symbol result = new Symbol();
         result.Type = TYPE.NUMERIC;
         result.DoubleValue = eval.DoubleValue;
         result.Name = "";
         return result;
-      }
-      else
-      {
+      } else {
         throw new Exception("Invalid type for unary minus");
       }
     }
 
-    public Symbol visit(RuntimeContext rtx, RelationalExpression r){
-      Symbol lEval= r.GetLExpression().accept(rtx, this);
+    public Symbol visit(RuntimeContext rtx, RelationalExpression r) {
+      Symbol lEval = r.GetLExpression().accept(rtx, this);
       Symbol rEval = r.GetRExpression().accept(rtx, this);
       RELATIONAL_OPERATOR op = r.GetOperator();
       Symbol result = new Symbol();
       result.Name = "";
       result.Type = TYPE.BOOL;
 
-      if(lEval.Type != rEval.Type){
+      if (lEval.Type != rEval.Type) {
         throw new Exception("Invalid types for relational operations");
       }
 
-      if(lEval.Type == TYPE.NUMERIC){
-        if(op == RELATIONAL_OPERATOR.EQUALITY)
+      if (lEval.Type == TYPE.NUMERIC) {
+        if (op == RELATIONAL_OPERATOR.EQUALITY)
           result.BooleanValue = lEval.DoubleValue == rEval.DoubleValue;
-        else if(op == RELATIONAL_OPERATOR.NOTEQUALITY)
+        else if (op == RELATIONAL_OPERATOR.NOTEQUALITY)
           result.BooleanValue = lEval.DoubleValue != rEval.DoubleValue;
-        else if(op == RELATIONAL_OPERATOR.GREATER_THAN)
+        else if (op == RELATIONAL_OPERATOR.GREATER_THAN)
           result.BooleanValue = lEval.DoubleValue > rEval.DoubleValue;
-        else if(op == RELATIONAL_OPERATOR.LESS_THAN)
+        else if (op == RELATIONAL_OPERATOR.LESS_THAN)
           result.BooleanValue = lEval.DoubleValue < rEval.DoubleValue;
-        else if(op == RELATIONAL_OPERATOR.GREATER_THAN_OR_EQUALITY)
+        else if (op == RELATIONAL_OPERATOR.GREATER_THAN_OR_EQUALITY)
           result.BooleanValue = lEval.DoubleValue >= rEval.DoubleValue;
-        else if(op == RELATIONAL_OPERATOR.LESS_THAN_OR_EQUALITY)
+        else if (op == RELATIONAL_OPERATOR.LESS_THAN_OR_EQUALITY)
           result.BooleanValue = lEval.DoubleValue <= rEval.DoubleValue;
         else
           throw new Exception("Invalid Relational Operator");
 
         return result;
 
-      } else if (lEval.Type == TYPE.STRING){
-        if(op == RELATIONAL_OPERATOR.EQUALITY)
-          result.BooleanValue = String.Compare(lEval.StringValue, rEval.StringValue)==0;
-        else if(op == RELATIONAL_OPERATOR.NOTEQUALITY)
-          result.BooleanValue = String.Compare(lEval.StringValue, rEval.StringValue)!=0;
+      } else if (lEval.Type == TYPE.STRING) {
+        if (op == RELATIONAL_OPERATOR.EQUALITY)
+          result.BooleanValue = String.Compare(lEval.StringValue, rEval.StringValue) == 0;
+        else if (op == RELATIONAL_OPERATOR.NOTEQUALITY)
+          result.BooleanValue = String.Compare(lEval.StringValue, rEval.StringValue) != 0;
         else
           result.BooleanValue = false;
         return result;
 
-      } else if (lEval.Type == TYPE.BOOL){
-        if(op == RELATIONAL_OPERATOR.EQUALITY)
+      } else if (lEval.Type == TYPE.BOOL) {
+        if (op == RELATIONAL_OPERATOR.EQUALITY)
           result.BooleanValue = lEval.BooleanValue == rEval.BooleanValue;
-        else if(op == RELATIONAL_OPERATOR.NOTEQUALITY)
+        else if (op == RELATIONAL_OPERATOR.NOTEQUALITY)
           result.BooleanValue = lEval.BooleanValue != rEval.BooleanValue;
         else
           result.BooleanValue = false;
@@ -254,18 +177,18 @@ namespace SLANG
       return null;
     }
 
-    public Symbol visit(RuntimeContext rtx, LogicalExpression l){
+    public Symbol visit(RuntimeContext rtx, LogicalExpression l) {
       Symbol lEval = l.GetLExpression().accept(rtx, this);
       Symbol rEval = l.GetRExpression().accept(rtx, this);
       TOKEN op = l.GetOperator();
 
-      if(lEval.Type == TYPE.BOOL && rEval.Type == TYPE.BOOL){
+      if (lEval.Type == TYPE.BOOL && rEval.Type == TYPE.BOOL) {
         Symbol result = new Symbol();
         result.Name = "";
         result.Type = TYPE.BOOL;
-        if(op ==TOKEN.AND)
+        if (op == TOKEN.AND)
           result.BooleanValue = lEval.BooleanValue && rEval.BooleanValue;
-        else if(op == TOKEN.OR)
+        else if (op == TOKEN.OR)
           result.BooleanValue = lEval.BooleanValue || rEval.BooleanValue;
         else
           return null;
@@ -275,10 +198,10 @@ namespace SLANG
       return null;
     }
 
-    public Symbol visit(RuntimeContext rtx,LogicalNot ln){
+    public Symbol visit(RuntimeContext rtx, LogicalNot ln) {
       Symbol eval = ln.GetExpression().accept(rtx, this);
 
-      if(eval.Type == TYPE.BOOL){
+      if (eval.Type == TYPE.BOOL) {
         Symbol result = new Symbol();
         result.Type = TYPE.BOOL;
         result.Name = "";
@@ -289,21 +212,21 @@ namespace SLANG
       }
     }
 
-    public Symbol visit(RuntimeContext rtx, IfStatement ifs){
+    public Symbol visit(RuntimeContext rtx, IfStatement ifs) {
       Symbol eval = ifs.GetCondition().accept(rtx, this);
-      if(eval.Type != TYPE.BOOL){
+      if (eval.Type != TYPE.BOOL) {
         throw new Exception("Condition evaluation failed");
       }
 
-      if(eval.BooleanValue == true){
+      if (eval.BooleanValue == true) {
         ArrayList statements = ifs.GetStatements("trueStatements");
-        foreach(Stmt s in statements){
+        foreach (Stmt s in statements) {
           s.accept(rtx, this);
         }
       } else {
         ArrayList statements = ifs.GetStatements("falseStatements");
-        if(statements != null){
-          foreach(Stmt s in statements){
+        if (statements != null) {
+          foreach (Stmt s in statements) {
             s.accept(rtx, this);
           }
         }
@@ -311,7 +234,7 @@ namespace SLANG
       return null;
     }
 
-    public Symbol visit( RuntimeContext rtx, WhileStatment ws ) {
+    public Symbol visit(RuntimeContext rtx, WhileStatment ws) {
     Wloop:
       Symbol condition = ws.GetCondition().accept(rtx, this);
       ArrayList statements = ws.GetStatements();
@@ -325,7 +248,7 @@ namespace SLANG
         Symbol tsp = null;
         foreach (Stmt s in statements) {
           tsp = s.accept(rtx, this);
-          if(tsp != null){
+          if (tsp != null) {
             return tsp;
           }
         }
@@ -333,10 +256,33 @@ namespace SLANG
       goto Wloop;
     }
 
-    public Symbol visit(RuntimeContext rtx, ReturnStatement rs){
+    public Symbol visit(RuntimeContext rtx, ReturnStatement rs) {
       Expression exp = rs.GetExpression();
       rs.inf = exp == null ? null : exp.accept(rtx, this);
       return rs.inf;
+    }
+
+    public Symbol visit(RuntimeContext rtx, CallProcedureExpression cpe) {
+      Procedure procedure = cpe.GetProcedure();
+      ArrayList actuals = cpe.GetActuals();
+      if (procedure != null) {
+        RuntimeContext cont = new RuntimeContext(rtx.GetModule());
+        ArrayList lst = new ArrayList();
+
+        foreach (Expression e in actuals) {
+          lst.Add(e.accept(rtx, this));
+        }
+        return procedure.Execute(cont, lst);
+      } else {
+        procedure = rtx.GetModule().FindProcedure(cpe.GetProcedureName());
+        RuntimeContext cont = new RuntimeContext(rtx.GetModule());
+        ArrayList lst = new ArrayList();
+
+        foreach (Expression e in actuals) {
+          lst.Add(e.accept(rtx, this));
+        }
+        return procedure.Execute(cont, lst);
+      }
     }
   }
 }
--- src/expression/BinaryExpressions.cs
+++ src/expression/BinaryExpressions.cs
@@ -1,194 +1,58 @@
 using System.Reflection.Emit;
 
-namespace SLANG
-{
-  public class BinaryPlus : Expression
-  {
+namespace SLANG {
+  public class BinaryExpression : Expression {
     private Expression _exp1, _exp2;
     private TYPE _type;
+    private OPERATOR _operator;
 
-    public BinaryPlus(Expression e1, Expression e2)
-    {
+    public BinaryExpression(Expression e1, Expression e2, OPERATOR op) {
       _exp1 = e1;
       _exp2 = e2;
+      _operator = op;
     }
 
-    public override Symbol accept(RuntimeContext cont,Visitor v)
-    {
-      return v.visit(cont,this);
+    public override Symbol accept(RuntimeContext cont, Visitor v) {
+      return v.visit(cont, this);
     }
 
-    public override bool Compile(DNET_EXECUTABLE_GENERATION_CONTEXT dtx)
-    {
+    public override bool Compile(DNET_EXECUTABLE_GENERATION_CONTEXT dtx) {
       _exp1.Compile(dtx);
       _exp2.Compile(dtx);
 
-      if(_type == TYPE.NUMERIC){
-        dtx.CodeOutput.Emit(OpCodes.Add);
-      }else{
+      if (_type == TYPE.NUMERIC) {
+        switch (_operator) {
+          case OPERATOR.PLUS: dtx.CodeOutput.Emit(OpCodes.Add); return true;
+          case OPERATOR.MINUS: dtx.CodeOutput.Emit(OpCodes.Sub); return true;
+          case OPERATOR.MULT: dtx.CodeOutput.Emit(OpCodes.Mul); return true;
+          case OPERATOR.DIV: dtx.CodeOutput.Emit(OpCodes.Div); return true;
+          default: throw new Exception("Invalid Operator");
+        }
+      } else if (_type == TYPE.STRING) {
         Type[] str2 = { typeof(string), typeof(string) };
         dtx.CodeOutput.Emit(OpCodes.Call, typeof(string).GetMethod("Concat", str2));
+        return true;
+      } else {
+        throw new Exception("Invalid types for binary operation");
       }
-      return true;
     }
 
-    public Expression GetLExpression() => _exp1;
-    public Expression GetRExpression() => _exp2;
-
-    public override TYPE TypeCheck(CompilationContext cont)
-    {
+    public override TYPE TypeCheck(CompilationContext cont) {
       TYPE lEval = _exp1.TypeCheck(cont);
       TYPE rEval = _exp2.TypeCheck(cont);
 
-      if (lEval == rEval && lEval != TYPE.BOOL)
-      {
+      if (lEval == rEval && lEval != TYPE.BOOL) {
         _type = lEval;
         return _type;
-      }
-      else
-      {
+      } else {
         throw new Exception("Invalid types for binary plus");
       }
     }
 
-    public override TYPE Get_Type()=> _type;
-  }
-
-  public class BinaryMinus : Expression
-  {
-    private Expression _exp1, _exp2;
-    private TYPE _type;
-
-    public BinaryMinus(Expression e1, Expression e2)
-    {
-      _exp1 = e1;
-      _exp2 = e2;
-    }
-
-    public override Symbol accept(RuntimeContext cont,Visitor v)
-    {
-      return v.visit(cont,this);
-    }
-
-    public override bool Compile(DNET_EXECUTABLE_GENERATION_CONTEXT dtx)
-    {
-      _exp1.Compile(dtx);
-      _exp2.Compile(dtx);
-      dtx.CodeOutput.Emit(OpCodes.Sub);
-      return true;
-    }
-
     public Expression GetLExpression() => _exp1;
     public Expression GetRExpression() => _exp2;
+    public OPERATOR GetOperator() => _operator;
+    public override TYPE Get_Type() => _type;
 
-    public override TYPE TypeCheck(CompilationContext cont)
-    {
-      TYPE lEval = _exp1.TypeCheck(cont);
-      TYPE rEval = _exp2.TypeCheck(cont);
-
-      if (lEval == rEval && lEval != TYPE.BOOL)
-      {
-        _type = lEval;
-        return _type;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary minus");
-      }
-    }
-
-    public override TYPE Get_Type()=> _type;
-  }
-
-  public class BinaryMultiplication : Expression
-  {
-    private Expression _exp1, _exp2;
-    private TYPE _type;
-
-    public BinaryMultiplication(Expression e1, Expression e2)
-    {
-      _exp1 = e1;
-      _exp2 = e2;
-    }
-
-    public Expression GetLExpression() => _exp1;
-    public Expression GetRExpression() => _exp2;
-
-    public override Symbol accept(RuntimeContext cont,Visitor v)
-    {
-      return v.visit(cont,this);
-    }
-
-    public override bool Compile(DNET_EXECUTABLE_GENERATION_CONTEXT dtx)
-    {
-      _exp1.Compile(dtx);
-      _exp2.Compile(dtx);
-      dtx.CodeOutput.Emit(OpCodes.Mul);
-      return true;
-    }
-
-    public override TYPE TypeCheck(CompilationContext cont)
-    {
-      TYPE lEval = _exp1.TypeCheck(cont);
-      TYPE rEval = _exp2.TypeCheck(cont);
-
-      if (lEval == rEval && lEval != TYPE.BOOL)
-      {
-        _type = lEval;
-        return _type;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary minus");
-      }
-    }
-
-    public override TYPE Get_Type()=> _type;
-  }
-
-  public class BinaryDivision : Expression
-  {
-    private Expression _exp1, _exp2;
-    private TYPE _type;
-
-    public BinaryDivision(Expression e1, Expression e2)
-    {
-      _exp1 = e1;
-      _exp2 = e2;
-    }
-
-    public Expression GetLExpression() => _exp1;
-    public Expression GetRExpression() => _exp2;
-
-    public override Symbol accept(RuntimeContext cont,Visitor v)
-    {
-      return v.visit(cont,this);
-    }
-
-    public override bool Compile(DNET_EXECUTABLE_GENERATION_CONTEXT dtx)
-    {
-      _exp1.Compile(dtx);
-      _exp2.Compile(dtx);
-      dtx.CodeOutput.Emit(OpCodes.Div);
-      return true;
-    }
-
-    public override TYPE TypeCheck(CompilationContext cont)
-    {
-      TYPE lEval = _exp1.TypeCheck(cont);
-      TYPE rEval = _exp2.TypeCheck(cont);
-
-      if (lEval == rEval && lEval != TYPE.BOOL)
-      {
-        _type = lEval;
-        return _type;
-      }
-      else
-      {
-        throw new Exception("Invalid types for binary minus");
-      }
-    }
-
-    public override TYPE Get_Type()=> _type;
   }
 }
