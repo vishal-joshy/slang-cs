5a50caa41eeef395debbd458bc49492318d1c651
vishaljoshy18 <1812-vishal@protonmail.com>
2022-07-07 07:53:41 +0530

    Fix formatting

--- Program.cs
+++ Program.cs
@@ -1,58 +1,61 @@
-﻿using System.Collections;
-using SLANG;
+﻿using SLANG;
 
 namespace Program
 {
-  class Program
-  {
-    static string ReadFile(string fileName)
+    class Program
     {
-      string result = "";
-      StreamReader sr = new StreamReader(fileName);
-      result = sr.ReadToEnd();
-      sr.Close();
-      return result;
-    }
-
-    static void RunCode(string code, string mode)
-    {
-      Console.WriteLine("code\n-------- \n" + code);
-
-      RDParser parser = new RDParser(code);
-      TModule module = parser.DoParse();
-      if(mode == "i"){
-        RUNTIME_CONTEXT rtx = new RUNTIME_CONTEXT(module);
-        SYMBOL symbol = module.Execute(rtx,null);
-      } else if (mode == "c")
-      {
-        if (module.CreateExecutable("test.exe"))
+        static string ReadFile(string fileName)
         {
-          Console.WriteLine("Executable Created");
-          return;
+            string result = "";
+            StreamReader sr = new StreamReader(fileName);
+            result = sr.ReadToEnd();
+            sr.Close();
+            return result;
         }
-      }
 
-    }
+        static void RunCode(string code, string mode)
+        {
+            Console.WriteLine(code);
 
-    static void Main(string[] args)
-    {
-      if (args.Length == 0)
-      {
-        Console.WriteLine("Usage: dotnet run <filename>");
-        return;
-      }
-      string code;
-      string filename = args[0];
-      try
-      {
-        code = ReadFile(filename);
-      }
-      catch
-      {
-        throw new Exception("Could not read file " + filename);
-      }
-      string mode = args.Length > 1 ? args[1] : "i";
-      RunCode(code, mode);
+            RDParser parser = new RDParser(code);
+            TModule module = parser.DoParse();
+            Console.WriteLine("Parse successful \n ---------\n");
+
+            if (mode == "i")
+            {
+                RUNTIME_CONTEXT rtx = new RUNTIME_CONTEXT(module);
+                module.Execute(rtx, null);
+            }
+            else if (mode == "c")
+            {
+                if (module.CreateExecutable("test.exe"))
+                {
+                    Console.WriteLine("Executable Created");
+                    return;
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: dotnet run <filename>");
+                return;
+            }
+            string code;
+            string filename = args[0];
+            try
+            {
+                code = ReadFile(filename);
+            }
+            catch
+            {
+                throw new Exception("Could not read file " + filename);
+            }
+            string mode = args.Length > 1 ? args[1] : "i";
+            RunCode(code, mode);
+        }
     }
-  }
 }
